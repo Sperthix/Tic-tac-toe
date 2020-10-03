abebf20064042b3cab69279b696e8d0610bf48c3
Sperthix <matusbuci@gmail.com>
2020-10-03 23:30:52 +0200

    prvy commit, ten folder tam asi nepatri, co? xd

---
+++ main.py
@@ -0,0 +1,10 @@
+pole = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
+
+def vypis_pole():
+    print(pole[0] + " " + pole[1] + " " + pole[2])
+    print(pole[3] + " " + pole[4] + " " + pole[5])
+    print(pole[6] + " " + pole[7] + " " + pole[8])
+
+vypis_pole()
