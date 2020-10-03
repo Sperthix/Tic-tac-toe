31d19d51069c3b46a00bf599ef29015522dac1df
Sperthix <matusbuci@gmail.com>
2020-10-03 23:52:05 +0200

    pridane dalsie funkcie pre zaciatok

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ def vypis_pole():
     print(pole[3] + " " + pole[4] + " " + pole[5])
     print(pole[6] + " " + pole[7] + " " + pole[8])
 
-vypis_pole()
+def start():
+    print("1 2 3")
+    print("4 5 6")
+    print("7 8 9")
+    tah()
+
+def tah():
+    print("zadaj poziciu svojho tahu: 1-9")
+    volba = input()
+
+start()
+
+
