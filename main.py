5670ffce3e2cdb2936b620c0b9b1ffc2ad5f6f4f
Sperthix <matusbuci@gmail.com>
2020-10-04 10:52:42 +0200

    pridane rozpoznanie vyhry a ukoncenie programu

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#hracia plocha
 pole = ["-", "-", "-",
         "-", "-", "-",
         "-", "-", "-"]
+
+vitaz = "-"
 
 def vypis_pole():
     print(pole[0] + " " + pole[1] + " " + pole[2])
@@ -15,6 +18,8 @@ def start():
 
 def tah():
     kontrola_vyhry()
+    if vitaz != "-":
+        return
     volba = input("zadaj poziciu svojho tahu: 1-9")
     volba = int(volba) - 1
     if (pole[volba] == "-"):
@@ -27,6 +32,8 @@ def tah():
 
 def tah2():
     kontrola_vyhry()
+    if vitaz != "-":
+        return
     volba2 = input("zadaj poziciu svojho tahu: 1-9")
     volba2 = int(volba2) - 1
     if (pole[volba2] == "-"):
@@ -39,7 +46,41 @@ def tah2():
 
 
 def kontrola_vyhry():
-    print()
+    riadok()
+    stlpec()
+    diagonal()
+    if vitaz == "X":
+        print("Hrac X vyhral, gratulujeme :)")
+    elif vitaz == "0":
+        print("Hrac 0 vyhral, gratulujeme :)")
+
+def riadok():
+    global vitaz
+    if pole[0] == pole[1] == pole[2] != "-":
+        vitaz = pole[0]
+    elif pole[3] == pole[4] == pole[5] != "-":
+        vitaz = pole[3]
+    elif pole[6] == pole[7] == pole[8] != "-":
+        vitaz = pole[6]
+    else: return
+
+def stlpec():
+    global vitaz
+    if pole[0] == pole[3] == pole[6] != "-":
+        vitaz = pole[0]
+    elif pole[1] == pole[4] == pole[7] != "-":
+        vitaz = pole[1]
+    elif pole[2] == pole[5] == pole[8] != "-":
+        vitaz = pole[2]
+    else: return
+
+def diagonal():
+    global vitaz
+    if pole[0] == pole[4] == pole[8] != "-":
+        vitaz = pole[0]
+    elif pole[2] == pole[4] == pole[6] != "-":
+        vitaz = pole[2]
+    else: return
 
 
 start()
